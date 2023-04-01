0cb01cc0b9effd473441e2d3ef70a2fec1f40225
niearlygul <twoface1126@gmail.com>
2023-04-01 17:08:55 +0900

    4/1 문자열 기본

--- basic/test2.py
+++ basic/test2.py
@@ -1,2 +1,6 @@
-ssam = "ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ"
-print(ssam)
+a=int(input("나이?:"))
+b=str(input("이름?:"))
+c=str(input("학교?:"))
+d=int(input("반?:"))
+e=int(input("번호?:"))
+print(str(a)+"세",b,str(c)+"중학교",str(d)+"반",str(e)+"번")1
